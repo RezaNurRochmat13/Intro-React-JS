13d488b0d4341c8dd9633719e87fedcb93b3262f
RezaNurRochmat13 <ilovedreamtheater13@gmail.com>
2017-12-15 13:16:27 +0700

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/* function formatName(user){
+    return user.firstName + ' '+ user.lastName;
+}
+
+const user = {
+    firstName: 'Harper',
+    lastName: 'Rubi'
+};
+ */
+const element = (
+    // <h1>Hello, {formatName(user)}!</h1>
+    <h2>Rizky ganteng sangat</h2>
+  );
+  
+
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+);
